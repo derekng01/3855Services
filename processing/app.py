090d1f8f2c2f1d3b10df51024987761fd53ec6e4
Derek Ng <48866991+derekng01@users.noreply.github.com>
2020-11-16 12:32:40 -0800

    Update app.py

--- processing/app.py
+++ processing/app.py
@@ -91,8 +91,8 @@ def populate_stats():
 
     timestamp = {"timestamp": stats_info['last_updated']}
 
-    get_reader_book = requests.get('{}/readers/adding_book'.format(app_config['eventstore']['url']),params=current_time)
-    get_reader_user = requests.get('{}/readers/user'.format(app_config['eventstore']['url']),params=current_time)
+    get_reader_book = requests.get('{}/readers/adding_book'.format(app_config['eventstore']['url']),params=timestamp)
+    get_reader_user = requests.get('{}/readers/user'.format(app_config['eventstore']['url']),params=timestamp)
 
 
 

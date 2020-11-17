39c7421ff71890d535fc8683f083a51cea37ae4f
Derek Ng <48866991+derekng01@users.noreply.github.com>
2020-11-17 01:06:12 -0800

    Update app.py

--- receiver/app.py
+++ receiver/app.py
@@ -11,8 +11,8 @@ import os
 
 if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
     print("In Test Environment")
-    app_conf_file = "/config/app_conf.yaml"
-    log_conf_file = "/config/log_conf.yaml"
+    app_conf_file = "/config/receiver/app_conf.yaml"
+    log_conf_file = "/config/receiver/log_conf.yaml"
 else:
     print("In Dev Environment")
     app_conf_file = "app_conf.yaml"

1143e83e9d99362ca333ec3fcca9e2cada48a5eb
Derek Ng <48866991+derekng01@users.noreply.github.com>
2020-11-19 14:17:34 -0800

    Update app.py

--- audit_log/app.py
+++ audit_log/app.py
@@ -93,7 +93,8 @@ def get_reader_user(index):
 app = connexion.FlaskApp(__name__, specification_dir='')
 CORS(app.app)
 app.app.config['CORS_HEADERS'] = 'Content-Type'
-app.add_api('lab7_api.yaml', base_path='/', strict_validation=True, validate_responses=True)
+app.add_api('lab7_api.yaml', base_path='/audit_log', strict_validation=True, validate_responses=True)
+
 
 if __name__ == '__main__':
     app.run(port=8110)
